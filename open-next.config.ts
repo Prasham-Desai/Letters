d2e137387da2f6d1afb0253674033d7c8f2303de
PrashamDesai <prashamdesai9114@gmail.com>
2026-06-29 13:10:06 +0530

    Deploy changes

---
+++ open-next.config.ts
@@ -0,0 +1,6 @@
+// default open-next.config.ts file created by @opennextjs/cloudflare
+import { defineCloudflareConfig } from "@opennextjs/cloudflare";
+
+export default defineCloudflareConfig({
+	// Default in-memory cache is fine for this app
+});
